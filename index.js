5f2c28a6a23649be4653a7e21fffc21d57440bb7
Jesus Saucedo <jsaucedo1937@gmail.com>
2024-09-25 16:17:57 -0700

    open-api-project finished

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const latitude = 38.028;
+const longitude = -121.8847;
+
+
 fetch('https://api.open-meteo.com/v1/forecast?latitude=38.028&longitude=-121.8847&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,wind_direction_10m,wind_gusts_10m&timezone=GMT')
    .then(response => {
        if (!response.ok) {
@@ -6,8 +10,24 @@ fetch('https://api.open-meteo.com/v1/forecast?latitude=38.028&longitude=-121.884
        return response.json();
    })
    .then(data => {
-       console.log('Weather Data:', data); // Log the weather data to the console
+       console.log('Weather Data:', data); 
+
+       // Extracting the first hour's data from weather API
+       const temp = data.hourly.temperature_2m[0];
+        const humidity = data.hourly.relative_humidity_2m[0];
+        const windSpeed = data.hourly.wind_speed_10m[0];
+        const windDirection = data.hourly.wind_direction_10m[0];
+        const windGusts = data.hourly.wind_gusts_10m[0];
+
+        
+        document.getElementById('temperature').innerText = `Temperature: ${temp} °C`;
+        document.getElementById('humidity').innerText = `Humidity: ${humidity}%`;
+        document.getElementById('wind-speed').innerText = `Wind Speed: ${windSpeed} km/h`;
+        document.getElementById('wind-direction').innerText = `Wind Direction: ${windDirection}°`;
+        document.getElementById('wind-gusts').innerText = `Wind Gusts: ${windGusts} km/h`;
    })
    .catch(error => {
        console.error('Fetch error:', error);
    });
+
+   
